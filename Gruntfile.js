ec69e4299ae1ee539e3e0565787200d8957bdfa7
Adam McArthur <adam@adammcarthur.net>
2014-10-05 18:25:54 +1100

    add validation prototype to grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,7 @@ module.exports = function (grunt) {
                         "lib/reset.js",
                         "lib/clear.js",
                         "lib/validations/methods.js",
+                        "lib/validations/validate.js",
                         "lib/send.js"
                     ]
                 }
